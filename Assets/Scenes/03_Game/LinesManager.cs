67ece051602b9d4f36abe25a0a000e22d2105072
Christian <christianle87@icloud.com>
2020-04-11 12:10:36 +0200

    Update images and add line logic

---
+++ Assets/Scenes/03_Game/LinesManager.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LinesManager : MonoBehaviour
+{
+
+    GameObject lineH1;
+    GameObject lineH2;
+    GameObject lineH3;
+
+    GameObject lineV1;
+    GameObject lineV2;
+    GameObject lineV3;
+
+    GameObject lineI1;
+    GameObject lineI2;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        {
+            lineH1 = GameObject.Find("Line_H1");
+            lineH1.SetActive(false);
+
+            lineH2 = GameObject.Find("Line_H2");
+            lineH2.SetActive(false);
+
+            lineH3 = GameObject.Find("Line_H3");
+            lineH3.SetActive(false);
+        }
+
+        {
+            lineV1 = GameObject.Find("Line_V1");
+            lineV1.SetActive(false);
+
+            lineV2 = GameObject.Find("Line_V2");
+            lineV2.SetActive(false);
+
+            lineV3 = GameObject.Find("Line_V3");
+            lineV3.SetActive(false);
+        }
+
+        {
+            lineI1 = GameObject.Find("Line_I1");
+            lineI1.SetActive(false);
+
+            lineI2 = GameObject.Find("Line_I2");
+            lineI2.SetActive(false);
+        }
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
