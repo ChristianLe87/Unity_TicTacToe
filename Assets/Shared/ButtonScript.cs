684518871f110c5b18d44aeb0513c9482c838a77
Christian <christianle87@icloud.com>
2020-04-11 12:47:17 +0200

    change button logic

---
+++ Assets/Shared/ButtonScript.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ButtonScript : MonoBehaviour
+{
+
+    public Sprite O_image;
+    public Sprite X_image;
+    public Sprite Null_image;
+
+    PlayerMachineManager playerMachineManager;
+
+    bool hasBeenPlayed = false;
+    public char buttonVal { get; private set; }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerMachineManager = GameObject.Find("PlayerMachineManager").GetComponent<PlayerMachineManager>();
+        this.GetComponent<Image>().sprite = Null_image;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void OnUserClick()
+    {
+        if(hasBeenPlayed == false)
+        {
+            hasBeenPlayed = true;
+            this.GetComponent<Image>().sprite = X_image;
+            buttonVal = 'X';
+            playerMachineManager.isPlayerPlaying = false;
+        }
+    }
+
+    public void OnMachineClick()
+    {
+        if (hasBeenPlayed == false)
+        {
+            hasBeenPlayed = true;
+            this.GetComponent<Image>().sprite = O_image;
+            buttonVal = 'O';
+            playerMachineManager.isPlayerPlaying = true;
+        }
+    }
+}
---
+++ Assets/Shared/ButtonsManager.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ButtonsManager : MonoBehaviour
+{
+    Button button0;
+    Button button1;
+    Button button2;
+
+    Button button3;
+    Button button4;
+    Button button5;
+
+    Button button6;
+    Button button7;
+    Button button8;
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+
+
+}
