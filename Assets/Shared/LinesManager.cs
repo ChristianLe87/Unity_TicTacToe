de5eeacd47558a53b3a017f9ccd9bf660442463b
Christian <christianle87@icloud.com>
2020-04-12 17:51:06 +0200

    add some logic to the buttons and lines

--- Assets/Shared/LinesManager.cs
+++ Assets/Shared/LinesManager.cs
@@ -61,8 +61,72 @@ public class LinesManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        // Line H1
+        if (isLineWin('X', 0, 1, 2))
+        {
+            lineH1.SetActive(true);
+        }
+        // Line H2
+        else if (isLineWin('X', 3, 4, 5))
+        {
+            lineH2.SetActive(true);
+        }
+        // Line H3
+        else if (isLineWin('X', 6, 7, 8))
+        {
+            lineH3.SetActive(true);
+        }
+
+        // Line V1
+        else if (isLineWin('X', 0, 3, 6))
+        {
+            lineV1.SetActive(true);
+        }
+        // Line V2
+        else if (isLineWin('X', 1, 4, 7))
+        {
+            lineV2.SetActive(true);
+        }
+        // Line V3
+        else if (isLineWin('X', 2, 5, 8))
+        {
+            lineV3.SetActive(true);
+
+        }
+
+        // Line I1
+        else if (isLineWin('X', 0, 4, 8))
+        {
+            lineI1.SetActive(true);
+        }
+        // Line I2
+        else if (isLineWin('X', 2, 4, 6))
+        {
+            lineI2.SetActive(true);
+        }
+
+
     }
+    
+    private bool isLineWin(char chr, int el1, int el2, int el3)
+    {
+        var arr = new Button[3] { buttons[el1], buttons[el2], buttons[el3] };
+        var result = arr
+            //.Select(x => x)
+            .Where(x=>x.GetComponent<ButtonScript>().buttonVal == chr)
+            .ToList();
+
+        if (result.Count() == 3)
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+
+
 
     public void ShowLine(string lineName)
     {
